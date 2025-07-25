e3d9ffa598ca8ba3a04daa5594045eed40e49dc2
blacXtar <salmanmohammad399@gmail.com>
2025-07-25 21:03:59 +0530

    added cors for http api

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const http = require('http');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const { initSocket } = require('./websocket/socket');
-const gameRoute = require('./routes/game.routes')
-const path = require('path');
-const { runGameLoop } = require('./services/gameManager.service');
-
+const { initSocket } = require("./websocket/socket");
+const gameRoute = require("./routes/game.routes");
+// const path = require('path');
+const { runGameLoop } = require("./services/gameManager.service");
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = initSocket(server)
+const io = initSocket(server);
 
 // Middleware
 // app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  cors({ origin: "https://crypto-crash.netlify.app/", credentials: true })
+);
 app.use(express.json());
-app.use('/api/v1',gameRoute)
-
+app.use("/api/v1", gameRoute);
 
 // Basic route
-app.get('/', (req, res) => {
- res.send("Hi from crashgame api")
+app.get("/", (req, res) => {
+  res.send("Hi from crashgame api");
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {console.log("MongoDB connected")
-    runGameLoop()
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    runGameLoop();
   })
-  .catch(err => console.error("Mongo error:", err));
-
+  .catch((err) => console.error("Mongo error:", err));
 
 // Start server
 const PORT = process.env.PORT || 5000;
